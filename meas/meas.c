718628771eba84d97642193a8583866c0c9466a7
viakko <redgogh@vip.qq.com>
2025-11-27 15:57:11 +0800

    meas: simplify flow by using direct exit for subcommands

--- meas/meas.c
+++ meas/meas.c
@@ -24,13 +24,13 @@ static size_t strlen_utf8(const char *str)
         return len;
 }
 
-static void show_version()
+static void version_and_exit(void)
 {
         printf("meas version: %s\n", MEAS_VERSION);
         exit(0);
 }
 
-static void show_help(void)
+static void help_and_exit(void)
 {
         printf(
             "Usage: meas [OPTIONS]\n"
@@ -43,18 +43,19 @@ static void show_help(void)
         exit(0);
 }
 
-static void show_strlen(int is_unicode, const char *str)
+static int strlen_and_exit(int is_unicode, const char *str)
 {
         printf("%zu\n", is_unicode ? strlen_utf8(str) : strlen(str));
+        return 0;
 }
 
 int main(int argc, char **argv)
 {
         struct argparser *ap;
-        struct option *help;
-        struct option *version;
-        struct option *str;
-        struct option *unicode;
+        struct option *opt_help;
+        struct option *opt_version;
+        struct option *opt_str;
+        struct option *opt_uni;
 
         ap = argparser_create();
         if (!ap) {
@@ -62,10 +63,10 @@ int main(int argc, char **argv)
                 exit(1);
         }
 
-        argparser_add0(ap, &help, "h", "help", "show this help message and exit", OP_NULL);
-        argparser_add0(ap, &version, "version", "version", "show current version", OP_NULL);
-        argparser_add1(ap, &str, "s", "str", "input string value", OP_NULL | OP_REQVAL);
-        argparser_add0(ap, &unicode, "u", "unicode", "use unicode parse string length", OP_NULL);
+        argparser_add0(ap, &opt_help, "h", "help", "show this help message and exit", OP_NULL);
+        argparser_add0(ap, &opt_version, "version", "version", "show current version", OP_NULL);
+        argparser_add1(ap, &opt_str, "s", "str", "input string value", OP_NULL | OP_REQVAL);
+        argparser_add0(ap, &opt_uni, "u", "unicode", "use unicode parse string length", OP_NULL);
 
         if (argparser_run(ap, argc, argv) != 0) {
                 fprintf(stderr, "%s\n", argparser_error(ap));
@@ -73,13 +74,20 @@ int main(int argc, char **argv)
                 exit(1);
         }
 
-        if (help) show_help();
-        if (version) show_version();
-        if (str) show_strlen(unicode != NULL, str->sval);
+        if (opt_help)
+                help_and_exit();
 
-        /* Default execute get unicode string length */
-        if (argparser_count(ap) > 0)
-                show_strlen(1, argparser_val(ap, 0));
+        if (opt_version)
+                version_and_exit();
+
+        if (opt_str || argparser_count(ap) > 0) {
+                if (!opt_str) {
+                        /* Default execute get unicode string length */
+                        strlen_and_exit(1, argparser_val(ap, 0));
+                } else {
+                        strlen_and_exit(opt_uni != NULL, opt_str->sval);
+                }
+        }
 
         argparser_free(ap);
 
