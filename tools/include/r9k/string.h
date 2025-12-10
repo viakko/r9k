2aa9b53d760c6e6b9a7cb6a648b04ffa19cb318f
viakko <redgogh@vip.qq.com>
2025-12-10 16:33:24 +0800

    string: use isspace() to check for blank strings

--- tools/include/r9k/string.h
+++ tools/include/r9k/string.h
@@ -7,15 +7,6 @@
 
 #include <string.h>
 
-static int streq(const char *s1, const char *s2)
-{
-        return strcmp(s1, s2) == 0;
-}
-
-static int strblank(const char *str)
-{
-        return !str || !*str ||
-                strspn("\r\t\n", str) == strlen(str);
-}
+int strblank(const char *str);
 
 #endif /* R9K_STRING_H_ */
--- tools/string.c
+++ tools/string.c
@@ -3,3 +3,18 @@
  * Copyright (c) 2025 viakko
  */
 #include <r9k/string.h>
+#include <ctype.h>
+
+int strblank(const char *str)
+{
+        if (!str)
+                return 1;
+
+        while (*str) {
+                if (!isspace(*str))
+                        return 0;
+                str++;
+        }
+
+        return 0;
+}
