fea1877e2ba14fc500be79e27ff3d43a3d4095b6
viakko <redgogh@vip.qq.com>
2025-12-16 15:29:36 +0800

    compiler_attrs: only __GNUC__ or __clang__ canbe use __attribute__()

--- include/r9k/compiler_attrs.h
+++ include/r9k/compiler_attrs.h
@@ -5,11 +5,16 @@
 #ifndef COMPILER_ATTRUBUTES_H_
 #define COMPILER_ATTRUBUTES_H_
 
-#define __attr_nonnull(...) __attribute__((nonnull(__VA_ARGS__)))
-#define __attr_unused       __attribute__((unused))
-#define __attr_noreturn     __attribute__((noreturn))
-#define __attr_printf(a, b) __attribute__((format(printf, a, b)))
-
-
+#if defined(__GNUC__) || defined(__clang__)
+#  define __attr_nonnull(...) __attribute__((nonnull(__VA_ARGS__)))
+#  define __attr_unused       __attribute__((unused))
+#  define __attr_noreturn     __attribute__((noreturn))
+#  define __attr_printf(a, b) __attribute__((format(printf, a, b)))
+#else
+#  define __attr_nonnull(...)
+#  define __attr_unused
+#  define __attr_noreturn
+#  define __attr_printf(a, b)
+#endif
 
 #endif /* COMPILER_ATTRUBUTES_H_ */
