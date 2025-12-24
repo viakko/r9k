2a3e240ae868e081e1fa8da10ae44437a3d984e6
viakko <redgogh@vip.qq.com>
2025-12-24 08:55:10 +0800

    clip: add --quiet option for quiet copy contents to clipboard

--- clip/clip.c
+++ clip/clip.c
@@ -20,12 +20,11 @@ static int read_callback(struct argparse *ap, struct option *opt)
                 PANIC("error: popen() failed\n");
 
         char *buf = readall(fp);
-        if (!buf)
-                PANIC("error: realall() failed\n");
+        if (buf) {
+                printf("%s", buf);
+                free(buf);
+        }
 
-        printf("%s", buf);
-
-        free(buf);
         pclose(fp);
 
         return 0;
@@ -43,6 +42,9 @@ static int write_callback(struct argparse *ap, struct option *opt)
         if (!buf)
                 PANIC("error: realall() failed\n");
 
+        if (!argparse_has(ap, "q"))
+                printf("%s", buf);
+
         fwrite(buf, 1, strlen(buf), fp);
 
         free(buf);
@@ -56,12 +58,14 @@ int main(int argc, char *argv[])
         struct argparse *ap;
         struct option *opt_read = NULL;
         struct option *opt_write = NULL;
+        struct option *opt_quiet = NULL;
 
         ap = argparse_create("clip", "1.0");
         PANIC_IF(!ap, "argparse initialize failed");
 
         argparse_add0(ap, &opt_read, "read", NULL, "read ontents in clipboard", read_callback, 0);
         argparse_add0(ap, &opt_write, "write", NULL, "write ontents to clipboard", write_callback, 0);
+        argparse_add0(ap, &opt_quiet, "q", "quiet", "quiet write to clipboard", NULL, 0);
 
         if (argparse_run(ap, argc, argv) != A_OK)
                 PANIC("%s\n", argparse_error(ap));
